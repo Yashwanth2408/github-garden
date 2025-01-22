5bf9f7c08c1bdfb304a1b65014dc27577b678f65
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2025-01-22 03:20:00 +0000

    improve readability of Kadane algorithm

--- algorithms/dp.py
+++ algorithms/dp.py
@@ -74,3 +74,21 @@ def lis_length(arr):
         else:
             tails[pos] = num
     return len(tails)
+
+def lcs(s, t):
+    """Longest Common Subsequence. O(mn) time and space.
+    Returns the subsequence string."""
+    m, n = len(s), len(t)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i-1][j-1] + 1 if s[i-1] == t[j-1] else max(dp[i-1][j], dp[i][j-1])
+    seq, i, j = [], m, n
+    while i > 0 and j > 0:
+        if s[i-1] == t[j-1]:
+            seq.append(s[i-1]); i -= 1; j -= 1
+        elif dp[i-1][j] > dp[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+    return ''.join(reversed(seq))
