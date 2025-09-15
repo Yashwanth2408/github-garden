f5e50489b60276fa4fe124dba75c2606bc1e177a
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2025-09-15 02:45:00 +0000

    add iterative version of trie search

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -156,3 +156,14 @@ def _counting_sort_digit(arr, exp):
         count[d] -= 1
         output[count[d]] = num
     return output
+
+def insertion_sort(arr):
+    """Insertion sort. O(n²) worst, O(n) best, O(1) space. Stable."""
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
