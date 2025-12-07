c2c00b6c29192510ee3b8e80cc1dec712735170a
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2025-12-07 13:51:00 +0000

    feat: add bubble sort to algorithms library

--- algorithms/graphs.py
+++ algorithms/graphs.py
@@ -696,3 +696,18 @@ def has_cycle_directed(graph):
         return False
 
     return any(dfs(n) for n in graph if color[n] == WHITE)
+
+def has_cycle_directed(graph):
+    """Detect cycle in directed graph via DFS coloring. O(V + E)."""
+    WHITE, GRAY, BLACK = 0, 1, 2
+    color = {n: WHITE for n in graph}
+
+    def dfs(node):
+        color[node] = GRAY
+        for nb in graph.get(node, []):
+            if color[nb] == GRAY or (color[nb] == WHITE and dfs(nb)):
+                return True
+        color[node] = BLACK
+        return False
+
+    return any(dfs(n) for n in graph if color[n] == WHITE)
