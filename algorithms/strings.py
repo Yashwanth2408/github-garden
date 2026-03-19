b15a34598c8755e860f6c477c7dde73f1f4c6b15
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2026-03-19 17:48:00 +0000

    add iterative version of prefix sum

--- algorithms/strings.py
+++ algorithms/strings.py
@@ -1854,3 +1854,31 @@ def min_remove_to_valid(s):
                 to_remove.add(i)
     to_remove.update(open_stack)
     return ''.join(ch for i, ch in enumerate(s) if i not in to_remove)
+
+def is_valid_brackets(s):
+    """Check balanced brackets. O(n) time, O(n) space."""
+    stack = []
+    match = {')': '(', ']': '[', '}': '{'}
+    for ch in s:
+        if ch in '([{':
+            stack.append(ch)
+        elif ch in match:
+            if not stack or stack[-1] != match[ch]:
+                return False
+            stack.pop()
+    return not stack
+
+
+def min_remove_to_valid(s):
+    """Remove minimum parens to make string valid. O(n)."""
+    open_stack, to_remove = [], set()
+    for i, ch in enumerate(s):
+        if ch == '(':
+            open_stack.append(i)
+        elif ch == ')':
+            if open_stack:
+                open_stack.pop()
+            else:
+                to_remove.add(i)
+    to_remove.update(open_stack)
+    return ''.join(ch for i, ch in enumerate(s) if i not in to_remove)
