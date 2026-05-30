54ae8584b8123c5bb526179d20329160052c094d
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2026-05-30 03:13:00 +0000

    optimize trie insertion space complexity

--- algorithms/strings.py
+++ algorithms/strings.py
@@ -2026,3 +2026,31 @@ def find_pattern_z(text, pattern):
     z = z_function(s)
     m = len(pattern)
     return [i - m - 1 for i in range(m + 1, len(s)) if z[i] == m]
+
+def is_valid_brackets(s):
+    """Check balanced brackets. O(n) time, O(n) space."""
+    stack = []
+    match = {')': '(', ']': '[', '}': '{'}
+    for ch in s:
+        if ch in '([{':
+            stack.append(ch)
+        elif ch in match:
+            if not stack or stack[-1] != match[ch]:
+                return False
+            stack.pop()
+    return not stack
+
+
+def min_remove_to_valid(s):
+    """Remove minimum parens to make string valid. O(n)."""
+    open_stack, to_remove = [], set()
+    for i, ch in enumerate(s):
+        if ch == '(':
+            open_stack.append(i)
+        elif ch == ')':
+            if open_stack:
+                open_stack.pop()
+            else:
+                to_remove.add(i)
+    to_remove.update(open_stack)
+    return ''.join(ch for i, ch in enumerate(s) if i not in to_remove)
