3cc0b5b4e42ed0dfcca4ff9dc0e63a4766a35ce3
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2026-06-18 07:18:00 +0000

    docs: add complexity analysis to interpolation search

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -46,3 +46,23 @@ def _heapify(arr, n, i):
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
         _heapify(arr, n, largest)
+
+def merge_sort(arr):
+    """Merge sort. O(n log n) time, O(n) space. Stable."""
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return _merge(left, right)
+
+
+def _merge(left, right):
+    result, i, j = [], 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i]); i += 1
+        else:
+            result.append(right[j]); j += 1
+    result.extend(left[i:]); result.extend(right[j:])
+    return result
