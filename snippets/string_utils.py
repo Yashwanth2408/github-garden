b9fb8a8fe3d9b121701de82b1a408a595e88b983
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2026-06-18 18:10:00 +0000

    expand string_utils: add format_bytes

--- snippets/string_utils.py
+++ snippets/string_utils.py
@@ -9,3 +9,9 @@ def slugify(text):
     text = re.sub(r'[^\w\s-]', '', text)
     text = re.sub(r'[\s_-]+', '-', text)
     return re.sub(r'^-+|-+$', '', text)
+
+def truncate(text, max_len, suffix='...'):
+    """Truncate text to max_len, appending suffix if cut."""
+    if len(text) <= max_len:
+        return text
+    return text[:max_len - len(suffix)] + suffix
