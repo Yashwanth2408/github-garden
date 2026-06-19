19cfbacbc4d877164a87357d661b2c282956eac9
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2026-06-19 17:10:00 +0000

    improve error handling in slugify

--- snippets/cli_utils.py
+++ snippets/cli_utils.py
@@ -7,3 +7,22 @@ def color_print(text, color='reset', bold=False):
     code = codes.get(color, 0)
     prefix = f'[{"1;" if bold else ""}{code}m'
     print(f'{prefix}{text}[0m')
+
+def table_print(rows, headers=None, min_width=3):
+    """Print a list of dicts or tuples as a formatted ASCII table."""
+    if not rows:
+        return
+    if isinstance(rows[0], dict):
+        if headers is None:
+            headers = list(rows[0].keys())
+        data = [[str(row.get(h, '')) for h in headers] for row in rows]
+    else:
+        data = [[str(c) for c in row] for row in rows]
+        if headers is None:
+            headers = [f'Col{i}' for i in range(len(data[0]))]
+    widths = [max(min_width, len(h), *(len(r[i]) for r in data)) for i, h in enumerate(headers)]
+    fmt = '  '.join(f'{{:<{w}}}' for w in widths)
+    print(fmt.format(*headers))
+    print('  '.join('-' * w for w in widths))
+    for row in data:
+        print(fmt.format(*row))
